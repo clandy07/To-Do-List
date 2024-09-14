db945bd091077bbf7d43a5d0ae79c33fbca4547a
clandy07 <edelcortes3@gmail.com>
2024-09-14 19:22:04 +0800

    Added the ability to add and display items on the list

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { SafeAreaView, View, TextInput, StyleSheet } from 'react-native';
-import { Appbar, Button } from 'react-native-paper';
+import { ScrollView } from 'react-native';
+import { Appbar, Button, Card, Text } from 'react-native-paper';
 
 export default function Index() {
+
+  const [input, setInput] = useState('');
+
+  const [notes, setNotes] = useState<string[]>([]);
+
+  const addNote = () => {
+    if(input.trim()=='') return;
+    setNotes([...notes, input]);
+    setInput('');
+  }
+
+
   return (
     <SafeAreaView style={styles.container}>
       <Appbar.Header>
@@ -10,9 +23,21 @@ export default function Index() {
       </Appbar.Header>
 
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} placeholder='List item'/>
-        <Button mode="contained">Add</Button>
+        <TextInput style={styles.input} placeholder='List item' value={input} onChangeText={setInput}/>
+        <Button onPress={addNote} mode="contained">Add</Button>
       </View>
+
+
+      <ScrollView style={styles.scrollView}>
+        {notes.map((note, index) => (
+          <Card key={index} style={styles.card}>
+            <Card.Content>
+              <Text>{note}</Text>
+            </Card.Content>
+          </Card>
+        ))}
+      </ScrollView>
+
     </SafeAreaView>
   )
 }
@@ -34,5 +59,11 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     marginRight: 10,
+  },
+  scrollView: {
+    padding: 10,
+  },
+  card: {
+    marginBottom: 10,
   },
 });
